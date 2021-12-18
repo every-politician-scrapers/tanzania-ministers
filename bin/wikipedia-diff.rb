307a68c7b8e6692b0adc333443574760699f01b5
Tony Bowden <tony@tmtm.com>
2021-12-18 11:53:32 +0200

    Skip Minister of State

--- bin/wikipedia-diff.rb
+++ bin/wikipedia-diff.rb
@@ -8,4 +8,5 @@ class Comparison < EveryPoliticianScraper::Comparison
 end
 
 diff = Comparison.new('data/wikidata-wp.csv', 'data/wikipedia.csv').diff
+  .reject { |row| row.last.to_s =~ /Ministers? of State/i }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
